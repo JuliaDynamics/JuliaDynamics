7c3a2c3299a75aa4909cf8155b0ff541fd858cde
Jared Howland <github@jaredhowland.com>
2019-02-03 18:07:14 -0700

    [ADD] Test run

---
+++ _codesnippets/build/test.jl
@@ -0,0 +1,8 @@
+using DynamicalBilliards, PyPlot
+
+bd = billiard_stadium()
+N = 20
+cs = [(i/N, 0, 1 - i/N, 0.5) for i in 1:N]
+ps = [Particle(1, 0.6 + 0.0005*i, 0) for i in 1:N]
+
+animate_evolution(ps, bd, 10.0; colors = cs, tailtime = 1.5)
