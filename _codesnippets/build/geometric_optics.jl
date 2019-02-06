9b5bd135c70ce23403675d13b196f9d6a0e42f52
George Datseris <datseris.george@gmail.com>
2019-02-06 20:32:09 +0100

    geometric optics snippet

---
+++ _codesnippets/build/geometric_optics.jl
@@ -0,0 +1,14 @@
+using DynamicalBilliards, PyPlot
+
+# Create a circular "lens"
+o = Antidot(SVector(1.0, 0.75), 0.5)
+# in a rectangular box
+bd = Billiard(billiard_rectangle(2.5, 1.5)..., o)
+
+trans, refra = law_of_refraction(1.5)
+rs = (RaySplitter([5], trans, refra),)
+
+ps = [Particle(0.1, y, 0.0) for y in 0.4:0.05:1.1]; N = length(ps)
+cs = [0.5 .* (0, i/N, 1 - i/N, 0.9) for i in eachindex(ps)]
+
+animate_evolution(ps, bd, 2.0, rs, tailtime=2.5, colors = cs)
