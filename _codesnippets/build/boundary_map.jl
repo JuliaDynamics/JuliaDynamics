f3427de484611f2e543b8b820892d803712a25c2
George Datseris <datseris.george@gmail.com>
2019-10-12 13:43:17 +0200

    add Agents.jl

--- _codesnippets/build/boundary_map.jl
+++ _codesnippets/build/boundary_map.jl
@@ -1,10 +1,12 @@
 using DynamicalBilliards, PyPlot
 
+rc = MushroomTools.randomchaotic
+rr = MushroomTools.randomregular
 bd = billiard_mushroom(1.0, 0.5, 1.0)
 
 n = 500 # how many particles to evolve
-ps = [MushroomTools.randomchaotic(1.0, 0.5, 1.0) for _ in 1:n÷2]
-append!(ps, [MushroomTools.randomregular(1.0, 0.5, 1.0) for _ in 1:n÷2])
+ps = [rc(1.0, 0.5, 1.0) for _ in 1:n÷2]
+append!(ps, [rr(1.0, 0.5, 1.0) for _ in 1:n÷2])
 bmap, arcs = parallelize(boundarymap, bd, 1000, ps)
 
 colors = [i > n÷2 ? "C2" : "k" for i in 1:n]
